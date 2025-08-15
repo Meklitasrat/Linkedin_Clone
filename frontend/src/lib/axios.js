46084d249805dff4d7d20eecb55c71b51c25514a
Meklit Asrat <148390664+Meklitasrat@users.noreply.github.com>
2025-08-15 11:15:39 +0300

    Done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // this will return axios instance that will enable us to fetch;
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: import.meta.mode === 'development' ? 'http://localhost:5000/api/v1':'/api/v1',
     // we add with credentials so as to pass cookies with our request to show authorized/authenticated accounts
     withCredentials: true
 });
